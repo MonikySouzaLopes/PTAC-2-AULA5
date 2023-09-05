3a30407aa0e1df506c24221ac41dd94f62f37e18
MonikySouzaLopes <monikysouzalopes@gmail.com>
2023-09-05 09:01:32 -0400

    Implementação de 3 atributos

--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -1,32 +1,48 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
+
 export default function ToDo() {
-   const [atividade, setAtividade ] = useState("");
+   const [tipo, setTipo ] = useState("");
+   const [marca, setMarca ] = useState("");
+   const [preco, setPreco ] = useState("");
    const [lista, setLista ] = useState([]);
    const [id,setId] = useState(1);
     const salvar =(e) =>{
         e.preventDefault();
         setLista([...lista, {
-                atividade: atividade,
+                tipo: tipo, marca:marca, preco:preco, 
+
                 id: id
         }]);
         setId(id + 1);
-        setAtividade("");
+        setTipo("");
+        setMarca("");
+        setPreco("");
     };
 
     return (
         <div>
             <Link to="/">home</Link>
-            <h1>Lista de Atividades</h1>    
+            <h1>Lista de produtos</h1>    
             <form onSubmit={salvar}>
-            <input value={atividade} type="text"
-            onChange={(e)=>{ setAtividade(e.target.value)}}/>
-            <button>ADD</button>   
+            <input value={tipo} type="text"
+            onChange={(e)=>{ setTipo(e.target.value)}}/>
+          &emsp;
+            <input value={marca} type="text"
+            onChange={(e)=>{ setMarca(e.target.value)}}/>
+           &emsp;
+            <input value={preco} type="text"
+            onChange={(e)=>{ setPreco(e.target.value)}}/>
+            &emsp;
+            <button>ADD</button>
             </form>      
             {lista.map((ativ)=>
             <div key= {ativ.id}>
-                <p>{ativ.atividade}</p>
+               
+                <p>Tipo:{ativ.tipo}</p>
+                <p>Marca:{ativ.marca}</p>
+                <p>Preço:R${ativ.preco}</p>
             </div>
             )} 
         </div>
