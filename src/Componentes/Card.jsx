e4fc7d752286624f9f0ab171bfab588e8f57e339
MonikySouzaLopes <monikysouzalopes@gmail.com>
2023-11-17 14:24:54 -0400

    Atualizado 17.11

---
+++ src/Componentes/Card.jsx
@@ -0,0 +1,9 @@
+export default function Card({ativ}){
+
+    return(
+        <div>
+            <h1>{ativ.ativ}</h1>
+        </div>
+    );
+
+}
--- src/Detalhe/detalhe.jsx
+++ src/Detalhe/detalhe.jsx
@@ -1,12 +1,20 @@
 import { useParams } from "react-router-dom";
-
+import Card from '..Componentes/Card';
 export default function Detalhe() {
 
     const {id} = useParams();
+    const lista = JSON.parse(localStorage.getItem("Lista"));
+    
+
+   const ativ =  lista.filter((objeto) => {
+        if(objeto.id == id){
+            return objeto;
+        }
+        return null
+    })
+console.log(ativ[0]);
     return(
-        <div>
-        <h1>Olá {id}</h1>
-        </div>
+        <Card ativ={ativ[0]}></Card>
     );
         
     
